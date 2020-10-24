a64d24fa7709f6a2e88a2155c977feb27b4b7e92
주in <juchan06@naver.com>
2020-10-24 23:15:41 +0900

    Update updown.py

--- updown.py
+++ updown.py
@@ -1,20 +1,23 @@
 from random import *
 
-print("업다운 게임")
+try:
+    print("업다운 게임")
 
-b = randrange(1, 101)
-cnt = 0
-while True:
-    a = int(input("1부터 100까지 숫자 중 하나를 입력하세요"))
-    if a < b:
-        print("Up")
-        cnt += 1
-    elif a > b:
-        print("Down")
-        cnt += 1
-    else:
-        cnt += 1
-        break
+    b = randrange(1, 101)
+    cnt = 0
+    while True:
+        a = int(input("1부터 100까지 숫자 중 하나를 입력하세요"))
+        if a < b:
+            print("Up")
+            cnt += 1
+        elif a > b:
+            print("Down")
+            cnt += 1
+        else:
+            cnt += 1
+            break
 
-print("정답!")
-print("{0}번 만에 맞췄습니다.".format(str(cnt)))
+    print("정답!")
+    print("{0}번 만에 맞췄습니다.".format(str(cnt)))
+except ValueError:
+    print("1부터 100까지의 숫자 중 하나를 입력해 주세요")
